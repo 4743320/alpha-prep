25a0af3943653929587e95b8594a3c06395d683b
4743320 <128045577+4743320@users.noreply.github.com>
2025-11-30 22:30:02 +0500

    new

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,56 +1,42 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
 import '../styles/new.css'
+import TestLogos from "../components/TestLogos";
+import AuthModal from "../components/AuthModal";
+import { UserContext } from "../Context/UserContext";
+import { userUser } from "../hooks/UseUser";
+import CourseCard from '../components/CourseCard'
+import IELTSLogo from "../assets/ieltslogo2.png"
+import SATLogo from "../assets/sat.png";
+import HeroImg from '../assets/Pics/hero.png'
+
 
 const New = () => {
+      const [showModal, setShowModal] = useState(false)
+      const [modalMode, setModalMode] = useState("signin")
+    
+      const{user, logOut} = userUser()
+    
   return (
-<div class="bg-white">
-    <header class="bg-[#FCF8F1] bg-opacity-30">
-        <div class="px-4 mx-auto sm:px-6 lg:px-8">
-            <div class="flex items-center justify-between h-16 lg:h-20">
-                <div class="flex-shrink-0">
-                    <a href="#" title="" class="flex">
-                        <img class="w-auto h-8" src="https://cdn.rareblocks.xyz/collection/celebration/images/logo.svg" alt="" />
-                    </a>
-                </div>
-
-                <button type="button" class="inline-flex p-2 text-black transition-all duration-200 rounded-md lg:hidden focus:bg-gray-100 hover:bg-gray-100">
-                    {/* <!-- Menu open: "hidden", Menu closed: "block" --> */}
-                    <svg class="block w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h16M4 16h16"></path>
-                    </svg>
-
-                    {/* <!-- Menu open: "block", Menu closed: "hidden" --> */}
-                    <svg class="hidden w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                    </svg>
-                </button>
-
-                <div class="hidden lg:flex lg:items-center lg:justify-center lg:space-x-10">
-                    <a href="#" title="" class="text-base text-black transition-all duration-200 hover:text-opacity-80"> Features </a>
-
-                    <a href="#" title="" class="text-base text-black transition-all duration-200 hover:text-opacity-80"> Solutions </a>
-
-                    <a href="#" title="" class="text-base text-black transition-all duration-200 hover:text-opacity-80"> Resources </a>
-
-                    <a href="#" title="" class="text-base text-black transition-all duration-200 hover:text-opacity-80"> Pricing </a>
-                </div>
-
-                <a href="#" title="" class="hidden lg:inline-flex items-center justify-center px-5 py-2.5 text-base transition-all duration-200 hover:bg-yellow-300 hover:text-black focus:text-black focus:bg-yellow-300 font-semibold text-white bg-black rounded-full" role="button"> Join Now </a>
-            </div>
-        </div>
-    </header>
-
+<div>
   <section class="hero-section">
   <div class="container">
     <div class="hero-grid">
       <div class="hero-text">
-        <p class="subtitle">A social media for learners</p>
-        <h1 class="title">Connect & learn from the experts</h1>
-        <p class="description">Grow your career fast with the right mentor.</p>
-        <a href="#" class="cta-button">Join for free</a>
+        {/* <p class="subtitle">A social media for learners</p> */}
+        <TestLogos/>
+        <h2 class="title">Study Smarter, Score Higher</h2>
+        <p class="description">Get exam-ready with <b><i>Free Official Practice Tests and Expert Study Resources</i></b> .
+We offer Proficiency Tests, Aptitude Tests, and accredited Professional Diplomas & Certifications — all designed to help you learn smarter and score higher.
+Study at your own pace with trusted material, helpful guidance, and a learning path built just for you.
+.</p>
+        {/* <a href="#" class="cta-button">Join for free</a> */}
+          <div className="hero-buttons">
+            <button className="primary-btn">Get Started</button>
+            <button className="primary-btn">Learn More</button>
+          </div>
       </div>
       <div class="hero-image">
-        <img src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/1/hero-img.png" alt="Hero" />
+        <img src={HeroImg} alt="Hero" />
       </div>
     </div>
   </div>
